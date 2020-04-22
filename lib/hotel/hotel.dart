bd69314687e8fc54e161c5528803e5a5233ca1f0
PresKhaled <pres.kbayomy@gmail.com>
2020-04-22 17:29:41 +0200

    Prepare Hotel UI to contribution

---
+++ lib/hotel/hotel.dart
@@ -0,0 +1,14 @@
+// Essential Flutter packages
+import 'package:flutter/material.dart';
+
+class Hotel extends StatefulWidget {
+  @override
+  _HotelState createState() => _HotelState();
+}
+
+class _HotelState extends State<Hotel> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: <Widget>[]));
+  }
+}
