a7c476e48dea08289cef0c72892ef8c2759863f4
PresKhaled <pres.kbayomy@gmail.com>
2020-04-22 19:59:52 +0200

    Prepare multiple UIs to contribution
    - Discover
    - Drawer
    - Travel

---
+++ lib/explore/discover.dart
@@ -0,0 +1,14 @@
+// Essential Flutter packages
+import 'package:flutter/material.dart';
+
+class Discover extends StatefulWidget {
+  @override
+  _DiscoverState createState() => _DiscoverState();
+}
+
+class _DiscoverState extends State<Discover> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(child: Column(children: <Widget>[])));
+  }
+}
---
+++ lib/travel/travel.dart
@@ -0,0 +1,14 @@
+// Essential Flutter packages
+import 'package:flutter/material.dart';
+
+class Travel extends StatefulWidget {
+  @override
+  _TravelState createState() => _TravelState();
+}
+
+class _TravelState extends State<Travel> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(child: Column(children: <Widget>[])));
+  }
+}
