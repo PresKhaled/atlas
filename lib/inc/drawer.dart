11e2fc901256d4f5c3ae2ffe2b18a866e0c76232
Ahmed Ragab <ragaba287@gmail.com>
2020-04-24 11:48:33 +0200

    Drawer

--- lib/inc/drawer.dart
+++ lib/inc/drawer.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 
-ListTile sideDrawerItem({IconData icon, String itemName, Function onPressed}) {
+//The used item for the drawer
+ListTile sideDrawerItem({Image image, String itemName, Function onPressed}) {
   return ListTile(
     title: Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
       child: Row(
         children: <Widget>[
-          Icon(
-            icon,
-            color: Colors.grey[600],
+          //Left icon
+          Container(
+            margin: EdgeInsets.only(right: 15),
+            height: 24,
+            width: 24,
+            child: image,
           ),
-          SizedBox(
-            width: 15,
-          ),
+
+          // The item text
           Text(
             itemName,
             style: TextStyle(
+                letterSpacing: 1.5,
                 fontSize: 15,
                 fontFamily: 'Montserrat',
                 fontWeight: FontWeight.w500),
@@ -23,6 +27,8 @@ ListTile sideDrawerItem({IconData icon, String itemName, Function onPressed}) {
         ],
       ),
     ),
+
+    //on press on the drawer item
     onTap: onPressed,
   );
 }
@@ -32,55 +38,133 @@ Drawer sideDrawer() {
     elevation: 10,
     child: ListView(
       children: <Widget>[
+        // Drawer Header => top container in the drawer
         DrawerHeader(
           child: Center(
             child: Column(
               children: <Widget>[
+                // The user profile and the check mark
                 Expanded(
-                  flex: 4,
-                  child: Container(
-                    margin: EdgeInsets.only(bottom: 5.0),
-                    decoration: BoxDecoration(boxShadow: <BoxShadow>[
-                      BoxShadow(
-                          offset: Offset(0, 5),
-                          color: Color.fromRGBO(0, 0, 0, .2),
-                          spreadRadius: 1,
-                          blurRadius: 10)
-                    ]),
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20.0),
-                        child: Image.asset("assets/images/profile-photo.png")),
+                  flex: 50,
+                  child: Stack(
+                    alignment: Alignment.bottomRight,
+                    children: <Widget>[
+                      //The user profile
+                      Container(
+                        decoration: BoxDecoration(boxShadow: <BoxShadow>[
+                          BoxShadow(
+                              offset: Offset(0, 5),
+                              color: Color.fromRGBO(0, 0, 0, .2),
+                              spreadRadius: 1,
+                              blurRadius: 10)
+                        ]),
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(25.0),
+                            child:
+                                Image.asset("assets/images/profile-photo.png")),
+                      ),
+
+                      //The check merk
+                      Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            gradient: LinearGradient(
+                              colors: [Color(0xffFA566A), Color(0xffE3317B)],
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                            ),
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                  offset: Offset(0, 5),
+                                  color: Color(0x60FA566A),
+                                  spreadRadius: .5,
+                                  blurRadius: 10)
+                            ]),
+                        height: 20,
+                        width: 20,
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Center(
+                              child: Image.asset(
+                                  'assets/images/drawer/check-mark.png')),
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                Text(
-                  'Bernard Cerny',
-                  style: TextStyle(
-                      fontFamily: 'Montserrat', fontWeight: FontWeight.w600),
+                Spacer(
+                  flex: 5,
                 ),
+
+                //The user name
                 Expanded(
-                    child: Text(
-                  'DJ/Traveler',
-                  style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.grey[600],
+                  flex: 13,
+                  child: Text(
+                    'Bernard Cerny',
+                    style: TextStyle(
+                      fontSize: 16,
+                      letterSpacing: 1,
+                      color: Color(0xff444444),
                       fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.w600),
-                )),
-                Expanded(
-                  child: RaisedButton(
-                    shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(18.0),
+                      fontWeight: FontWeight.w600,
                     ),
-                    onPressed: () {},
-                    color: Color(0xffE72E73),
-                    textColor: Colors.white,
+                  ),
+                ),
+
+                //The user sub text (job)
+                Expanded(
+                    flex: 15,
                     child: Text(
-                      "Edit",
+                      'Traveler',
                       style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.w600,
-                        fontSize: 15,
-                        color: Colors.white,
+                          fontSize: 12,
+                          letterSpacing: 3,
+                          color: Colors.grey[600],
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w600),
+                    )),
+
+                //The edit button
+                Expanded(
+                  flex: 20,
+                  child: Container(
+                    width: 80,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      padding: EdgeInsets.all(0.0),
+                      child: Ink(
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [Color(0xffFA566A), Color(0xffE3317B)],
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                            ),
+                            borderRadius: BorderRadius.circular(30.0)),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color(0x60FA566A),
+                                blurRadius: 10.0,
+                                spreadRadius: 0.5,
+                                offset: Offset(1.0, 2.0),
+                              )
+                            ],
+                          ),
+                          constraints:
+                              BoxConstraints(maxWidth: 80.0, minHeight: 50.0),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Edit",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.w600),
+                          ),
+                        ),
                       ),
                     ),
                   ),
@@ -92,29 +176,47 @@ Drawer sideDrawer() {
             color: Color(0xffEBEBEB),
           ),
         ),
+
+        // The item
         sideDrawerItem(
-            icon: Icons.explore, itemName: 'Discover', onPressed: () {}),
+            image: Image.asset('assets/images/drawer/discover.png'),
+            itemName: 'Discover',
+            onPressed: () {}),
         sideDrawerItem(
-            icon: Icons.near_me, itemName: 'Near me', onPressed: () {}),
+            image: Image.asset('assets/images/drawer/near-me.png'),
+            itemName: 'Near me',
+            onPressed: () {}),
         sideDrawerItem(
-            icon: Icons.apps, itemName: 'Activites', onPressed: () {}),
-        sideDrawerItem(icon: Icons.map, itemName: 'Map', onPressed: () {}),
+            image: Image.asset('assets/images/drawer/activities.png'),
+            itemName: 'Activities',
+            onPressed: () {}),
         sideDrawerItem(
-            icon: Icons.book, itemName: 'Bookings', onPressed: () {}),
+            image: Image.asset('assets/images/drawer/map.png'),
+            itemName: 'Map',
+            onPressed: () {}),
         sideDrawerItem(
-            icon: Icons.subscriptions,
+            image: Image.asset('assets/images/drawer/bookings.png'),
+            itemName: 'Bookings',
+            onPressed: () {}),
+        sideDrawerItem(
+            image: Image.asset('assets/images/drawer/subscription.png'),
             itemName: 'Subscription',
             onPressed: () {}),
         Divider(
+          height: 30,
           thickness: 2,
           endIndent: 55,
           indent: 55,
           color: Colors.grey[300],
         ),
         sideDrawerItem(
-            icon: Icons.settings, itemName: 'Settings', onPressed: () {}),
+            image: Image.asset('assets/images/drawer/settings.png'),
+            itemName: 'Settings',
+            onPressed: () {}),
         sideDrawerItem(
-            icon: Icons.exit_to_app, itemName: 'Logout', onPressed: () {}),
+            image: Image.asset('assets/images/drawer/logout.png'),
+            itemName: 'Logout',
+            onPressed: () {}),
       ],
     ),
   );
